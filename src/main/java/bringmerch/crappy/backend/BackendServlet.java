6008f34fe5809558749c78cd53e6a1c81eacb5b8
bringmerch <lgop8327@gmail.com>
2026-06-24 17:46:59 +0900

    PrintWriter 사용

--- src/main/java/bringmerch/crappy/backend/BackendServlet.java
+++ src/main/java/bringmerch/crappy/backend/BackendServlet.java
@@ -5,14 +5,17 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "backendServlet", value = "/") // "/" = 디폴트서블릿으로 작동
+@WebServlet("/work")
 public class BackendServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         response.setCharacterEncoding("UTF-8");
         response.addCookie(new Cookie("age", "31"));
-        String sb = """
+        response.setStatus(HttpServletResponse.SC_OK);
+        // body
+        PrintWriter writer = response.getWriter();
+        writer.println("""
                 <!DOCTYPE html>
                     <html lang="en">
                     <head>
@@ -31,8 +34,8 @@ public class BackendServlet extends HttpServlet {
                         <h1>roger that.</h1>
                     </body>
                     </html>
-            """;
-        response.getWriter().write(sb);
+            """);
+        // content-length 헤더는 tomcat이 세팅함
     }
 
 }
